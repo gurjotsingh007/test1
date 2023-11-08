d9311ef7865d463c8e872ff08db9b6f3e125a28d
Gurjot Singh <101056456+gurjotsingh007@users.noreply.github.com>
2023-11-08 13:11:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ async function startServer() {
       app.use(express.static(path.join(__dirname, '/frontend/build')));
 
       app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')
+        res.sendFile(path.resolve(__dirname, '/frontend/build/index.html')
       ));
     } else {
       app.get('/', (req, res) => {
