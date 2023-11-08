782dce220a715098872d1d1c55f5e6ea86d0ffe8
Gurjot Singh <101056456+gurjotsingh007@users.noreply.github.com>
2023-11-08 16:02:45 +0530

    added protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,35 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Heading from './Components/Heading/Heading';
-import Body from './Components/Body/Body';
+import Body from "./Components/Body/Body"
 import Footer from './Components/Footer/Footer';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import Login from './Components/LoginSignUp/Login';
 import Signup from './Components/LoginSignUp/SignUp';
 import Logout from './Components/Logout/Logout';
-
+import { useEffect } from 'react';
 function App() {
-  const hasToken = !!localStorage.getItem('token');
-
+  let token = null;
+  useEffect(() => {
+    token = localStorage.getItem('token');
+    console.log("useEffect called");
+  },[])
   return (
     <Router>
       <>
-        <Heading />
-        <Routes>
-          <Route path="/" element={<Body />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          {hasToken ? (
-            <Route path="/logout" element={<Logout />} />
-          ) : (
-            <Navigate to="/signIn" replace />
-          )}
-          <Route path="/signIn" element={<Login />} />
-          <Route path="/signUp" element={<Signup />} />
-        </Routes>
-        <Footer />
+      <Heading/>
+      <Routes>
+        <Route path="/" element={<Body/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signUp" element={<Signup/>} />
+        {token!==null &&
+        <Route path="/logout" element={<Logout/>} />
+        }
+      </Routes>
+      <Footer/>
       </>
     </Router>
   );
